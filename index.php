ae4aa7944a9c39a9d83a70e5d9de5aa5cefbc567
JuanDavidZabalaTapiero <juandavidzabalatapiero@gmail.com>
2024-07-26 18:41:46 -0500

    Función para invertir los caracteres de un texto

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
 
+
+echo '<h2>100 primeros números primos</h2> <br>';
+
 // 4. Función que muesta los 100 primeros números primos
-function main() {
+function main()
+{
     for ($number = 1; $number <= 100; $number++) {
         if (isPrime($number)) {
             echo $number . PHP_EOL;
@@ -9,7 +13,8 @@ function main() {
     }
 }
 
-function isPrime($number) {
+function isPrime($number)
+{
     if ($number < 2) {
         return false;
     }
@@ -24,3 +29,23 @@ function isPrime($number) {
 }
 
 main();
+
+echo '<br> <h2>Inviriendo cadenas de texto</h2> <br>';
+
+// 7. Invertir cadena de texto
+function reverseString($input)
+{
+    // Convertimos la cadena en un array de caracteres
+    $characters = str_split($input);
+    // Invertimos el array
+    $reversedCharacters = array_reverse($characters);
+    // Unimos los caracteres invertidos en una cadena
+    $reversedString = implode('', $reversedCharacters);
+    return $reversedString;
+}
+
+// Ejemplo de uso
+$input = "Hola mundo";
+$output = reverseString($input);
+echo $output; // odnum aloH
+
